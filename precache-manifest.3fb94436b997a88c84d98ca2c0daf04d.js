527106c48934a42586bce6cc91f84002e5dfdc9f
suhas <suhas525252@gmail.com>
2019-05-07 10:43:14 +0530

    busigence react

---
+++ precache-manifest.3fb94436b997a88c84d98ca2c0daf04d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "746cdd13d90744429897dcc7ea75ec75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47f6929d33db64488f14",
+    "url": "/static/js/2.45aa7e9c.chunk.js"
+  },
+  {
+    "revision": "eb5c7c6f7ab7d80645df",
+    "url": "/static/js/main.7116e17e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
